16cf767dba269237eae2917b8bbda246cad1d03f
bethac07 <bcimini@broadinstitute.org>
2023-04-17 14:07:40 -0400

    initial app

---
+++ cellprofiler_suggester.py
@@ -0,0 +1,15 @@
+import pandas
+import streamlit as st
+
+q_dataframe = pandas.read_csv('bioimage_object_analysis_schema.csv')
+q_questions = list(q_dataframe.columns)
+
+
+with st.form(key='this_form'):
+    for question in range(len(q_questions)):
+        globals()[f'option_{question}'] = st.selectbox(q_questions[question],q_dataframe[q_questions[question]].dropna())
+
+    submit_button = st.form_submit_button(label='Submit')
+
+for question in range(len(q_questions)):
+    st.write(globals()[f'option_{question}'])
