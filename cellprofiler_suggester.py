76c1f6117b50455a91496a6f9fd1df4093811f90
bethac07 <bcimini@broadinstitute.org>
2023-04-17 14:20:19 -0400

    use multiselect where appropriate

--- cellprofiler_suggester.py
+++ cellprofiler_suggester.py
@@ -4,10 +4,12 @@ import streamlit as st
 q_dataframe = pandas.read_csv('bioimage_object_analysis_schema.csv')
 q_questions = list(q_dataframe.columns)
 
-
 with st.form(key='this_form'):
     for question in range(len(q_questions)):
-        globals()[f'option_{question}'] = st.selectbox(q_questions[question],q_dataframe[q_questions[question]].dropna())
+        if 'all that apply' in q_questions[question]:
+            globals()[f'option_{question}'] = st.multiselect(q_questions[question],q_dataframe[q_questions[question]].dropna())
+        else:
+            globals()[f'option_{question}'] = st.selectbox(q_questions[question],q_dataframe[q_questions[question]].dropna())
 
     submit_button = st.form_submit_button(label='Submit')
 
