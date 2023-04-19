60b57c30c1eb61f62e0c0ebfbc25461f02e9101b
Beth Cimini <bethac07@users.noreply.github.com>
2023-04-19 15:47:25 -0400

    Update home.py

--- home.py
+++ home.py
@@ -8,4 +8,5 @@ Can plan to do this multipage - https://docs.streamlit.io/library/get-started/mu
 - one with CellProfiler suggestions.
 Need also a util data loader function, pull all the jsons and put them into (likely a few) master dicts with the right components
 It's all just tiny text files so it should be fine, but https://docs.streamlit.io/library/api-reference/performance/st.cache_data
+Embed in webpage rather than webpage forwarding - https://giswqs.medium.com/add-a-custom-domain-to-your-streamlit-web-app-daed6d11dd72
 """
