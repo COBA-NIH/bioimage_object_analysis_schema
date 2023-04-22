ed6a448addc8da7aa329a5d09ba06a294954112b
Beth Cimini <bethac07@users.noreply.github.com>
2023-04-22 13:41:41 -0400

    Update Home.py

--- Home.py
+++ Home.py
@@ -14,7 +14,7 @@ Embed in webpage rather than webpage forwarding - https://giswqs.medium.com/add-
 """
 # Welcome to the home page for the "Twenty Questions of Bioimage Object Analysis" project!
 
-This is the part of the schema currently loaded into our "CellProfiler Suggester" tool; stay tuned for more updates.
+This is the part of the schema currently loaded into this interactive tool; stay tuned for more updates.
 The whole schema is currently best found on [Zenodo](https://doi.org/10.5281/zenodo.7654937)
 
 Last updated - April 22, 2023
