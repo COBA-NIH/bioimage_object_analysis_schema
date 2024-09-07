86940233054b87bdcb9c12c62c89d6059b79caea
Beth Cimini <bethac07@users.noreply.github.com>
2024-09-07 11:10:05 +0200

    Move Google Analytics directly into the app (#4)
    * Delete index.html
    
    * Create google_analytics.html
    
    * Add analytics directly to the app
    
    * Update 1_Schema_Definitions.py
    
    * Update 2_Figure.py
    
    * Update 3_Interactive_Schema.py
    
    * Update 4_CellProfiler_Pipeline_Suggester.py
    
    * Reorder
    
    * Path is rel to top app page, not local page, I guess
    
    * Remove all the things
    
    * cleanup
    
    * Fix page titles

--- pages/2_Figure.py
+++ pages/2_Figure.py
@@ -1,3 +1,5 @@
 import streamlit as st
 
+st.set_page_config(page_title='Bioimage Object Analysis Questions - BOAQ')
+
 st.image('figure.png')
--- pages/3_Interactive_Schema.py
+++ pages/3_Interactive_Schema.py
@@ -4,6 +4,8 @@ import sys
 
 import streamlit as st
 
+st.set_page_config(page_title='Bioimage Object Analysis Questions - BOAQ')
+
 sys.path.append('..')
 from bioimage_object_analysis_schema.utils import display_interactive_schema
 
