89aa3371dcd3b6e88e538acd772fa52d206d8780
Beth Cimini <bethac07@users.noreply.github.com>
2024-09-07 09:46:27 +0200

    Add analytics directly to the app

--- Home.py
+++ Home.py
@@ -1,18 +1,16 @@
 """
-Pages
-- One with nice expanded answers - done
-- Generic interactive page - done, but would be nice to load saved jsons
-- Recommender pages - 
--- CellProfiler - done
-- one with chart (charts.py) - TODO
--- mermaid chart maybe? 
--- https://docs.bokeh.org/en/latest/docs/examples/topics/categorical/periodic.html
--- just matplotlib sub-charts (https://matplotlib.org/stable/gallery/subplots_axes_and_figures/subfigures.html) with text boxes(https://matplotlib.org/stable/gallery/text_labels_and_annotations/fancytextbox_demo.html#sphx-glr-gallery-text-labels-and-annotations-fancytextbox-demo-py)), 
-
 It's all just tiny text files so it should be fine, but if need to cache- https://docs.streamlit.io/library/api-reference/performance/st.cache_data
-Embedded in a webpage at BOAQ.org, thanks to https://giswqs.medium.com/add-a-custom-domain-to-your-streamlit-web-app-daed6d11dd72
+v1 was Embedded in a webpage at BOAQ.org, thanks to https://giswqs.medium.com/add-a-custom-domain-to-your-streamlit-web-app-daed6d11dd72
+
+Current analytics approach thanks to https://discuss.streamlit.io/t/google-analytics-integration/41412/2
 """
 import streamlit as st
+import streamlit.components.v1 as components
+
+# Include Google Analytics tracking code
+with open("google_analytics.html", "r") as f:
+    html_code = f.read()
+    components.html(html_code, height=0)
 
 st.set_page_config(layout='wide')
 
@@ -24,7 +22,7 @@ This website is associated with the paper "The Twenty Questions of Bioimage Obje
 This schema is currently under development; stay tuned for more updates.
 Previous versions of this schema are currently best found on [Zenodo](https://doi.org/10.5281/zenodo.7654937)
 
-Last updated - July 11, 2023
+Last updated - September 7, 2024
 
 App made by Beth Cimini, Broad Institute, 2023, as part of the Center for Open Bioimage Analysis
 
