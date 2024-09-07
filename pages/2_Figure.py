fa51bc27f8cef3daedfb2aadd4d200eef79eba7d
Beth Cimini <bethac07@users.noreply.github.com>
2024-09-07 09:47:13 +0200

    Update 2_Figure.py

--- pages/2_Figure.py
+++ pages/2_Figure.py
@@ -1,3 +1,9 @@
 import streamlit as st
+import streamlit.components.v1 as components
+
+# Include Google Analytics tracking code
+with open("../google_analytics.html", "r") as f:
+    html_code = f.read()
+    components.html(html_code, height=0)
 
 st.image('figure.png')
